66f3d7717f0f09490c22941284487c9a01c65e5b
amant2312 <amant2312@gmail.com>
2025-01-04 12:17:17 +0530

    Testing different request handlers.

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ const express= require("express")
 const app=express();
 
 //app.use('/',(req,res)=>{ res.send("hello world"); });
-app.use('/test',(req,res)=>{ res.send("hello world from test"); });
-app.use('/dev',(req,res)=>{ res.send("hello world from dev"); });
+app.use('/test',[(req,res,next)=>{ next(); /* res.send("hello world from 1st")*/}, (req,res)=>{ res.send("hello world from 2nd"); }]);
 
 app.listen(3000,()=>{console.log("listening");
 })
