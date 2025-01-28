faeab0a387d715824cc860c1576b1071f0db2330
amant2312 <amant2312@gmail.com>
2025-01-28 19:46:14 +0530

    Data Validation using Validator library

--- src/app.js
+++ src/app.js
@@ -24,6 +24,32 @@ app.get('/user', async (req, res)=>{
     
 });
 
+app.patch("/user", async (req, res)=> {
+    const id= req.body.userId;
+    const data= req.body;
+    try{
+        const ALLLOWED_UPDATES= ["photoUrl", "about", "gender", "age", "skills"];
+        const isUpdateAllowed= Object.keys(data).every((k)=>
+            ALLLOWED_UPDATES.includes(k)
+        );
+        
+        if(!isUpdateAllowed){
+            throw new Error("Email update is not allowed");
+        }
+        
+        const user= await User.findByIdAndUpdate(id, data, {
+            returnDocument: true,
+            runValidators:true
+        });
+        console.log(user);
+        res.send("User updated successfully");
+    }
+    catch(error){
+        res.status(400).send("Something went wrong in the update: " + error.message);
+    }
+});
+
+
 app.post("/signup", async (req, res)=> {
     
     const user = new User(req.body);
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose= require("mongoose");
+const validator = require('validator');
 const userSchema = new mongoose.Schema(
     {
       firstName: {
@@ -16,6 +17,11 @@ const userSchema = new mongoose.Schema(
         required: true,
         unique: true,
         trim: true,
+        validate(value){
+          if(!validator.isEmail(value)){
+              throw new Error("Invalid Email Id");
+          }
+        }
       },
       password: {
         type: String,
