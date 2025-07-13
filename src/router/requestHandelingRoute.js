da27ef05674d259c4d0dccb3d5fbe267853d7b99
amant2312 <amant2312@gmail.com>
2025-07-13 12:26:40 +0530

    Added review connection request API Handler.

--- src/router/requestHandelingRoute.js
+++ src/router/requestHandelingRoute.js
@@ -5,12 +5,9 @@ const User= require('../models/user.js');
 const {userAuth}= require('../middlewares/auth.js');
 
 requestHandelingRouter.post('/request/send/:status/:toUserId',userAuth,async (req,res)=>{
-    
-
-
     try{
         const toUserId= req.params.toUserId;
-        const user= User.findById(toUserId);
+        const user= await User.findById(toUserId);
 
         if(!user){
             throw new Error("Requested person does not exist");
@@ -49,5 +46,25 @@ requestHandelingRouter.post('/request/send/:status/:toUserId',userAuth,async (re
     }
 });
 
+requestHandelingRouter.post('/request/review/:status/:requestId',userAuth,async (req,res)=>{
+    try{
+        const {status, requestId}= req.params;
+        const validStatus= ["accepted", "rejected"];
+        if(!validStatus.includes(req.params.status)){
+            throw new Error("Invalid status request");
+        }
+        const connection= await Connection.findOne({_id: requestId, toUserId: req.user._id, status: 'interested'});
+        if(!connection){
+            throw new Error("Request does not exist");
+        }
+        connection.status= status;
+        const retConn=await connection.save();
+        res.status(200).json({message: "Success", success: true});
+    }
+    catch(error){
+        res.status(404).json({message: error.message});
+    }
+    
+});
 
 module.exports= requestHandelingRouter;
